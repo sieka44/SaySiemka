f6443adf9ce5552c24442082aec8f6cd2d9e09fe
Barbara <barbara.karwowska11@gmail.com>
2018-01-02 23:38:48 +0100

    GUI

--- src/main/java/saysiemka/GUI/Main.java
+++ src/main/java/saysiemka/GUI/Main.java
@@ -1,7 +1,22 @@
 package saysiemka.GUI;
 
-/**
- * Created by barba on 18.12.2017.
- */
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/communicator_fxml.fxml"));
+        primaryStage.setTitle("Welcome");
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
